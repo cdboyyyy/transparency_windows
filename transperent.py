16d99dcbcf64e8fece772a9dcb7cefb80fd7cb85
Eran Sidi <eran_cd@outlook.com>
2023-02-24 22:30:12 +0000

    change

--- transperent.py
+++ transperent.py
@@ -25,7 +25,7 @@ def enum_windows_callback(hwnd, lparam):
             else:
                 win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
                 # winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 180, win32con.LWA_ALPHA)
-                winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 255, win32con.LWA_ALPHA)
+                winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 200, win32con.LWA_ALPHA)
         # print(class_name)
 
 while True:
