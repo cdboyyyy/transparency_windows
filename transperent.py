84b2473c131da3fe3291dc917b5d1c926020e92b
Eran Sidi <eran_cd@outlook.com>
2023-02-24 22:39:15 +0000

    change

--- transperent.py
+++ transperent.py
@@ -15,18 +15,23 @@ def enum_windows_callback(hwnd, lparam):
     title = win32gui.GetWindowText(hwnd)
     # Get the window class name
     class_name = win32gui.GetClassName(hwnd)
+    
+    def transperent(dim):
+        win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
+        winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), dim, win32con.LWA_ALPHA)
+    
     window_name = ''    
     if win32gui.IsWindowVisible(hwnd) == 1:
         correct_window = GetWindowText(GetForegroundWindow())
         if class_name in list:
             if correct_window == title:
-                win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
-                winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 255, win32con.LWA_ALPHA)
+                transperent(255)
+                # win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
+                # winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 255, win32con.LWA_ALPHA)
             else:
-                time.sleep(5)
-                win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
-                # winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 180, win32con.LWA_ALPHA)
-                winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 200, win32con.LWA_ALPHA)
+                transperent(255)
+                # win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
+                # winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 200, win32con.LWA_ALPHA)
         # print(class_name)
 
 while True:
